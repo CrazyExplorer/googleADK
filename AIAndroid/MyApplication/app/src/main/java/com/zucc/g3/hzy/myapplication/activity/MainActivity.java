b993bec353caacaf9578e3b0037e5dde805a04ac
CrazyExplorer <herculeshu777@gmail.com>
2018-07-26 21:46:51 +0800

    动用蓝牙服务
    动用蓝牙服务

--- AIAndroid/MyApplication/app/src/main/java/com/zucc/g3/hzy/myapplication/activity/MainActivity.java
+++ AIAndroid/MyApplication/app/src/main/java/com/zucc/g3/hzy/myapplication/activity/MainActivity.java
@@ -34,10 +34,25 @@ import java.util.HashMap;
 
 import java.util.Map;
 
-
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+
+
+
+
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattService;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
+import com.zucc.g3.hzy.myapplication.bth.BluetoothService;
+import com.zucc.g3.hzy.myapplication.bth.conn.BleCharacterCallback;
+import com.zucc.g3.hzy.myapplication.bth.exception.BleException;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 import static com.zucc.g3.hzy.myapplication.setting.ImageSetting.MAXHEIGHT;
 import static com.zucc.g3.hzy.myapplication.setting.ImageSetting.MAXWIDTH;
@@ -59,7 +74,7 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
         setContentView(R.layout.activity_main);
 
         initDebug();
-//        initBlueTooth();
+        initBlueTooth();
     }
 
     @Override
@@ -88,7 +103,7 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
         if (openCvCameraView != null) {
             openCvCameraView.disableView();
         }
-//        unbindService();
+        unbindService();
     }
 
     private void initDebug() {
@@ -129,6 +144,169 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
         PermissionUtils.requestPermissionsResult(this, requestCode, permissions, grantResults, mPermissionGrant);
         initCamera();
     }
+
+
+
+    /**
+     * BlueTooth
+     */
+
+
+    private BluetoothService mBluetoothService;
+    private BluetoothGatt gatt;
+    private BluetoothGattService service;
+
+    private void initBlueTooth() {
+        bindService();
+    }
+
+    private Handler bthHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == 1) {
+                getService();
+                BLE_start_listener();
+            } else if (msg.what == 2) {
+                BLE_start_writer();
+            }
+            super.handleMessage(msg);
+        }
+    };
+
+    public String protocol(int protocol_data) {
+        String protocol_msg = "";
+        if (protocol_data >= 0 && 100 > protocol_data) {
+            protocol_msg = Integer.toHexString(30) + Integer.toHexString(protocol_data + 30);
+        } else if (protocol_data < 10000 && protocol_data >= 100) {
+            protocol_msg = Integer.toHexString(protocol_data / 100 + 30) + Integer.toHexString(protocol_data % 100 + 30);
+        }
+        return protocol_msg;
+    }
+
+    private void writer(int controlMode, int targetL, int targetR) { //仅仅用于发送小车动作控制命令
+        final BluetoothGattCharacteristic characteristic = mBluetoothService.getCharacteristic();
+        mBluetoothService.write(
+                characteristic.getService().getUuid().toString(),
+                characteristic.getUuid().toString(),
+                String.valueOf(protocol(controlMode) + protocol(targetL) + protocol(targetR) + protocol(0) + protocol(0) + protocol(0) + protocol(0) + protocol(0) + protocol(0) + protocol(4321)),//发送10进制比特
+                new BleCharacterCallback() {
+                    @Override
+                    public void onSuccess(final BluetoothGattCharacteristic characteristic) {
+                        //成功写入操作
+                    }
+
+                    @Override
+                    public void onFailure(final BleException exception) {
+                        StartBLEWriterAfter(150);
+                    }
+
+                    @Override
+                    public void onInitiatedResult(boolean result) {
+
+                    }
+                });
+    }
+
+    private void StartBLEListenerAfter(int time) {
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            public void run() {
+                Message msg = new Message();
+                msg.what = 1;
+                bthHandler.sendMessage(msg);
+            }
+        };
+        timer.schedule(task, time);
+    }
+
+    private void StartBLEWriterAfter(int time) {
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            public void run() {
+                Message msg = new Message();
+                msg.what = 2;
+                bthHandler.sendMessage(msg);
+            }
+        };
+        timer.schedule(task, time);
+    }
+
+    private void getService() {
+        gatt = mBluetoothService.getGatt();
+        mBluetoothService.setService(gatt.getServices().get(gatt.getServices().size() - 1));
+    }
+
+    private void BLE_start_writer() {
+        service = mBluetoothService.getService();
+        mBluetoothService.setCharacteristic((service.getCharacteristics().get(service.getCharacteristics().size() - 2)));
+        mBluetoothService.setCharaProp(1);
+    }
+
+    private void BLE_start_listener() {
+        service = mBluetoothService.getService();
+        mBluetoothService.setCharacteristic((service.getCharacteristics().get(service.getCharacteristics().size() - 1)));
+        mBluetoothService.setCharaProp(2);
+        final BluetoothGattCharacteristic characteristic = mBluetoothService.getCharacteristic();
+        mBluetoothService.notify(
+                characteristic.getService().getUuid().toString(),
+                characteristic.getUuid().toString(),
+                new BleCharacterCallback() {
+
+                    @Override
+                    public void onSuccess(final BluetoothGattCharacteristic characteristic) {
+                        MainActivity.this.runOnUiThread(() -> {
+                        });
+                    }
+
+                    @Override
+                    public void onFailure(final BleException exception) {
+                        MainActivity.this.runOnUiThread(() -> {
+                            StartBLEListenerAfter(100);//重新开始监听
+                        });
+                    }
+
+                    @Override
+                    public void onInitiatedResult(boolean result) {
+
+                    }
+                });
+    }
+
+    private void bindService() {
+        Intent bindIntent = new Intent(this, BluetoothService.class);
+        this.bindService(bindIntent, mFhrSCon, Context.BIND_AUTO_CREATE);
+        StartBLEListenerAfter(100);
+        StartBLEWriterAfter(150);
+    }
+
+    private ServiceConnection mFhrSCon = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            mBluetoothService = ((BluetoothService.BluetoothBinder) service).getService();
+            mBluetoothService.setConnectCallback(callback);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mBluetoothService = null;
+        }
+    };
+
+    private BluetoothService.Callback2 callback = this::finish;
+
+    private void unbindService() {
+        this.unbindService(mFhrSCon);
+    }
+
+
+
+
+
+
+
+
+
+
 
     /**
      * OpenCV
@@ -218,7 +396,7 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
 //                writer(0, 0, 0);
             } else if (msg.what == 1) {
                 displayDebugMsg.setText("直行");
-//                writer(5, 50, 50);
+                writer(5, 50, 50);
             }
             super.handleMessage(msg);
         }
