c21d77183c527c4f1b061c9990bbe10494989d2a
CrazyExplorer <herculeshu777@gmail.com>
2018-08-07 17:39:49 +0800

    蓝牙骨架
    蓝牙骨架

---
+++ AIAndroid/AICar/app/src/main/java/org/blackwalnutlabs/hzy/aicar/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package org.blackwalnutlabs.hzy.aicar.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.blackwalnutlabs.hzy.aicar.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
